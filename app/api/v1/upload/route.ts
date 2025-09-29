a6bd96fe9c9f733fe8ca4238adb721af070d1c12
OpenSource <opensource@thearcadia.xyz>
2025-09-29 02:00:49 +0200

    Remove unnecessary export of runtime in upload route

--- app/api/v1/upload/route.ts
+++ app/api/v1/upload/route.ts
@@ -6,7 +6,7 @@ import { auth } from "@clerk/nextjs/server";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-export const runtime = "edge";
+
 
 export interface FileUploadRequest {
   filename: string;
